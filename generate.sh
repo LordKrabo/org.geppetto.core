ab7c1a013e99567e4198998b25cbacb05d92c99c
James Scheibner <jamess00@OSX-Base-Image-with-apps-v90.local>
2016-05-30 23:20:13 +1000

    Further changes to generate.sh with rsync modified flags rv

--- generate.sh
+++ generate.sh
@@ -91,7 +91,7 @@ generate_application() {
   #rsync -r --exclude=.git --delete $OUTPUT_DIR/ $REMOTE_DIR/
   #rsync -r --exclude=.git --delete-after $OUTPUT_DIR/ $REMOTE_DIR/
   #rsync -rt --delete --exclude=".git" --exclude=".travis.yml" $OUTPUT_DIR/ $REMOTE_DIR/ 
-  rsync -az --delete --exclude".git" ../_site/ .
+  rsync -rv --delete --exclude.git ../$OUTPUT_DIR/* .
   #rsync -r --exclude=.git --delete $OUTPUT_DIR/ ./
   #rsync -r --exclude=.git --delete ../$OUTPUT_DIR/ .
   #rsync -r --exclude=.git
